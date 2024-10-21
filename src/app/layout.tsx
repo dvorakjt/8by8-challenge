5dd1bbf015bf98b1466b23aba3f908214890da86
Joseph Dvorak <dvorakjt@gmail.com>
2024-10-20 20:44:18 -0400

    added meta description tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,8 @@ interface RootLayoutProps {
 
 export const metadata: Metadata = {
   title: '8by8 Challenge',
+  description:
+    'Join us in promoting civic engagement to combat hate against the AAPI community. Register to vote, sign up for election reminders, and share your challenge to earn badges. Together, we can make a difference! Made by 8by8.us with ❤️',
   openGraph: {
     images: {
       url: '/static/images/open-graph/thumbnail.png',
